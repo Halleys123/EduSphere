c7f39a2b4fc4c2b44107d5f639bb613afe4d5c3d
Halleys123 <65032719+Halleys123@users.noreply.github.com>
2023-06-05 01:47:53 +0530

    classes frontend and assignment backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,12 @@
+const assignmentRoute = require("./routes/assignmentRoute");
+
 const cors = require("cors");
 const express = require("express");
 const app = express();
 
 app.use(cors());
-// app.use(express.static("public"));
+app.use(express.json());
+
+app.use("/api/v1", assignmentRoute);
 
 module.exports = app;
