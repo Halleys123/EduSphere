6f0cbc4bfddb80bdbd6d9909f6e5d54e83864356
Halleys123 <65032719+Halleys123@users.noreply.github.com>
2023-07-06 16:16:50 +0530

    Added dropdowns and several other changes

---
+++ Frontend/pages/dashboard/dashboard.js
@@ -0,0 +1,8 @@
+// const button = document.querySelector(".main__nav__pageName");
+// // const dropDown = document.querySelector(".main__nav__pageName__dropdown");
+// const icon = document.querySelector(".main__nav__pageName__iconBox");
+
+// button.addEventListener("click", () => {
+//   //   dropDown.classList.toggle("hidden");
+//   icon.classList.toggle("hide");
+// });
