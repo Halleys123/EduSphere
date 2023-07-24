45245003ef02665807ed968c988aa34fe507bce8
Halleys123 <65032719+Halleys123@users.noreply.github.com>
2023-07-24 19:09:27 +0530

    Several error fix

---
+++ Frontend/pages/attend/js.js
@@ -0,0 +1,35 @@
+var options = {
+  series: [4, 32, 2],
+  labels: ["Absence", "Presence", "Unknown"],
+  chart: {
+    type: "donut",
+    sparkline: {
+      enabled: true,
+    },
+  },
+  plotOptions: {
+    pie: {
+      donut: {
+        labels: {
+          show: true,
+          total: {
+            showAlways: false,
+            show: true,
+            label: "Total",
+          },
+        },
+      },
+    },
+  },
+};
+
+var charts = [];
+var chart;
+
+document
+  .querySelectorAll(".main__content__box__details__chart")
+  .forEach((e) => {
+    chart = new ApexCharts(e, options);
+    charts.push(chart);
+    chart.render();
+  });
