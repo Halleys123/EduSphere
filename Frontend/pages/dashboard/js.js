842b5d68857845e94fbd1ff85423c8dbc34c6c9a
Halleys123 <65032719+Halleys123@users.noreply.github.com>
2023-07-25 22:27:19 +0530

    some more changes

--- Frontend/pages/dashboard/js.js
+++ Frontend/pages/dashboard/js.js
@@ -1,6 +1,7 @@
 let subjects = ["EE-101", "EE-102", "EE-103", "EE-104", "EE-105", "EE-108"];
 let attendence = [10, 18, 13, 15, 20, 17];
 let array = []; // Initialize array as an empty array
+
 attendence.forEach((e, i) => {
   array.push({ x: subjects[i], y: attendence[i] });
 });
@@ -85,62 +86,6 @@ var options = {
     },
   },
 };
-// Sample data for the bar chart
-// const chartData = {
-//   categories: ["Category 1", "Category 2", "Category 3", "Category 4"],
-//   series: [
-//     {
-//       name: "Series 1",
-//       data: [44, 55, 41, 67],
-//     },
-//     {
-//       name: "Series 2",
-//       data: [13, 23, 20, 8],
-//     },
-//   ],
-// };
-
-// Chart options
-// const chartOptions = {
-//   chart: {
-//     type: "bar",
-//     height: 350,
-//     stacked: false,
-//   },
-//   plotOptions: {
-//     bar: {
-//       horizontal: false,
-//       columnWidth: "55%",
-//     },
-//   },
-//   dataLabels: {
-//     enabled: false,
-//   },
-//   stroke: {
-//     show: true,
-//     width: 2,
-//     colors: ["transparent"],
-//   },
-//   xaxis: {
-//     categories: chartData.categories,
-//   },
-//   yaxis: {
-//     title: {
-//       text: "Values",
-//     },
-//   },
-//   fill: {
-//     opacity: 1,
-//   },
-//   tooltip: {
-//     y: {
-//       formatter: function (val) {
-//         return val;
-//       },
-//     },
-//   },
-// };
-
 // Create the chart
 const chart = new ApexCharts(
   document.querySelector(".main__content__attendance"),
