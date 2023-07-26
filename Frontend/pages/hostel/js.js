c82da0cfebbd23eb5adc95d80ccab9ec34a2157d
himanshu gupta <himanshugupta@himanshus-MacBook-Air.local>
2023-07-26 05:33:56 +0530

    some changes

--- Frontend/pages/hostel/js.js
+++ Frontend/pages/hostel/js.js
@@ -1,6 +1,18 @@
+common();
 fetch("http://127.0.0.1:3000/api/v1/mess", {
   method: "GET",
   headers: {
     Authentication: `Bearer ${localStorage.getItem("token")}`,
   },
-});
+})
+  .then((response) => response.json())
+  .then((data) => {
+    let html = ``;
+    data.message.data.mess.updates.forEach((item) => {
+      html += itemGenerate(item);
+    });
+    document.querySelector(".").innerHTML = "";
+    document
+      .querySelector(".main__content__assignments__items")
+      .insertAdjacentHTML("beforeend", html);
+  });
